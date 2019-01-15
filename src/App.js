4cb294c034d98a80c321de92d2d5cd9424c4aeb1
Yuka <ym@bidmotion.com>
2019-01-15 22:18:28 +0100

    Setup project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import Table from './components/Table';
+
+import { Form, Col, Input, Button } from 'antd';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      position: '',
+      email: '',
+      skype: '',
+      skypeUrl: '',
+      phone: ''
+    };
+  }
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value
+    });
+  };
+
+  resetSignature = () => {
+    this.setState({
+      firstName: '',
+      lastName: '',
+      position: '',
+      email: '',
+      skype: '',
+      skypeUrl: '',
+      phone: ''
+    })
+  }
+
   render() {
+    const formItemLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 14 },
+    };
     return (
-      <div className="App">
+      <div className="App" style={{ width: '500px', margin: 'auto' }}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>ReFuel Box Email Signature</h1>
+          <p>Please fill the form below to generate your signature</p>
         </header>
+        <Form layout="vertical" style={{ marginBottom: '20px' }}>
+          <Form.Item {...formItemLayout} label="First Name">
+            <Input name="firstName" onChange={this.handleChange} value={this.state.firstName} style={{ width: '100%'}}/>
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Last Name">
+            <Input name="lastName" onChange={this.handleChange} value={this.state.lastName} />
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Position">
+            <Input name="position" onChange={this.handleChange} value={this.state.position} />
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Email">
+            <Input name="email" onChange={this.handleChange} value={this.state.email} />
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Phone">
+            <Input name="phone" onChange={this.handleChange} value={this.state.phone} />
+          </Form.Item>
+          <Form.Item {...formItemLayout} label="Skype">
+            <Input name="skype" onChange={this.handleChange} value={this.state.skype} />
+          </Form.Item>
+          <Button onClick={this.resetSignature}>Reset Signature</Button>
+        </Form>
+        <Table
+            firstName={this.state.firstName}
+            lastName={this.state.lastName}
+            position={this.state.position}
+            email={this.state.email}
+            skype={this.state.skype}
+            skypeUrl={this.state.skypeUrl}
+            phone={this.state.phone}
+          />
       </div>
     );
   }
---
+++ src/components/Table.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import twitter from '../img/twitter.png';
+import linkedin from '../img/linkedin.png';
+import facebook from '../img/facebook.png';
+
+const info = {
+    linkedin: 'https://www.linkedin.com/company/refuelbox',
+    twitter: 'https://twitter.com/refuelbox',
+    facebook: 'https://www.facebook.com/refuelbox',
+    color: 'skyblue',
+    companyName: 'ReFuel Box',
+    companyWebsite: 'http://www.refuelbox.com',
+    logo: 'https://refuelbox.com/wp-content/uploads/2019/01/Email-signature-512x512_09.png',
+    logoLink: 'http://www.refuelbox.com'
+};
+
+const Table = ({ firstName, lastName, position, email, skype, phone }) => (
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <a href={info.logoLink}>
+                            <img src={info.logo} alt="Logo" height="80" width="80" />
+                        </a>
+                        <div style={{ textAlign: 'center' }}>
+                            <a href={info.twitter} target="_blank">
+                                <img src={twitter} alt="" width="16px" height="16px" style={{ marginRight: '4px' }} />
+                            </a>
+                            <a href={info.linkedin} target="_blank">
+                                <img src={linkedin} alt="" width="16px" height="16px" style={{ marginRight: '4px' }} />
+                            </a>
+                            <a href={info.facebook} target="_blank">
+                                <img src={facebook} alt="" width="16px" height="16px" />
+                            </a>
+                        </div>
+                    </td>
+                    <td><div style={{ display: 'block', borderLeft: '2px solid #272727', marginLeft: '5px', marginRight: '10px', height: '100px'}}></div></td>
+                    <td>
+                        <div>
+                            <div style={{ marginBottom: '10px'}}>
+                                <span style={{ fontWeight: 'bold' }}>{ firstName } { lastName }</span>
+                                <span style={{ display: 'block', fontSize: '11px' }}>{ position }</span>
+                            </div>
+                            <div style={{ fontSize: '11px'}}>
+                                <span style={{ display: 'block'}}>Email: <a href={'mailto:' + email} style={{ color: info.color}}>{ email }</a></span>
+                                <span style={{ display: 'block'}}>Skype: <span style={{ color: info.color}}>{ skype }</span></span>
+                                <span style={{ display: 'block'}}>Phone: <span style={{ color: info.color}}>{ phone }</span></span>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+         </table>
+    );
+
+export default Table
